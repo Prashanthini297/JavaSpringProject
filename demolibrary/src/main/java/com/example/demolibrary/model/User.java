26d9bdfb5d572c051d18e4051eb23d0221e2b62b
Prashanthini297 <156071320+Prashanthini297@users.noreply.github.com>
2025-05-13 15:44:28 +0530

    Add files via upload

---
+++ demolibrary/src/main/java/com/example/demolibrary/model/User.java
@@ -0,0 +1,72 @@
+package com.example.demolibrary.model;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany
+    private List<Book> borrowedBooks;
+    private Date borrowedDate;
+    private Date returnedDate;
+
+    public User() {}
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Book> getBorrowedBooks() {
+		return borrowedBooks;
+	}
+
+	public void setBorrowedBooks(List<Book> borrowedBooks) {
+		this.borrowedBooks = borrowedBooks;
+	}
+
+	public Date getBorrowedDate() {
+		return borrowedDate;
+	}
+
+	public void setBorrowedDate(Date borrowedDate) {
+		this.borrowedDate = borrowedDate;
+	}
+
+	public Date getReturnedDate() {
+		return returnedDate;
+	}
+
+	public void setReturnedDate(Date returnedDate) {
+		this.returnedDate = returnedDate;
+	}
+
+	public User(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+}
+
+
